48e39f9cb7bcbe0e42ec680b39bc8639ce95d9ce
AlfonsDer3te <48153808+AlfonsDer3te@users.noreply.github.com>
2020-03-06 20:20:44 +0100

    added explicit conversion in line 154
    Xcode was complaining

--- base64.h
+++ base64.h
@@ -1,7 +1,7 @@
-//
-//  base64 encoding and decoding with C++.
-//  Version: 1.01.00
-//
+	//
+	//  base64 encoding and decoding with C++.
+	//  Version: 1.01.00
+	//
 #pragma once
 #ifndef BASE64_H_C0CE2A47_D10E_42C9_A27C_C883944E704A
 #define BASE64_H_C0CE2A47_D10E_42C9_A27C_C883944E704A
@@ -15,15 +15,15 @@
 
 namespace Encoding {
 
-    class Base64 {
-    public:
-        static std::string Encode(std::string data, bool toUrlSafe = false, size_t lineBreaks = NO_LINE_BREAKS);
-        static std::string Decode(std::string encoded_string);
-        static std::string InsertLineBreaks(std::string base64, size_t lineBreakAppear = LINE_BREAKS_APPEAR_MIME);
-        static std::string RemoveLineBreaks(std::string base64);
-        static std::string ToSafeUrlBase64(std::string base64);
-        static std::string ToUnsafeUrlBase64(std::string base64);
-    private:
-    };
+class Base64 {
+public:
+	static std::string Encode(std::string data, bool toUrlSafe = false, size_t lineBreaks = NO_LINE_BREAKS);
+	static std::string Decode(std::string encoded_string);
+	static std::string InsertLineBreaks(std::string base64, size_t lineBreakAppear = LINE_BREAKS_APPEAR_MIME);
+	static std::string RemoveLineBreaks(std::string base64);
+	static std::string ToSafeUrlBase64(std::string base64);
+	static std::string ToUnsafeUrlBase64(std::string base64);
+private:
+};
 }
 #endif /* BASE64_H_C0CE2A47_D10E_42C9_A27C_C883944E704A */
